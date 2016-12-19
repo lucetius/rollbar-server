8343bf80d663d69fe1d9105ffaf7ab4779ecdfa0
lucetius <lucetius@lucetius.pl>
2016-12-20 00:28:03 +0100

    Fixed package visibility to server side

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lucetius:rollbar-server',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Rollbar error reporting for Meteor server side',
   git: 'https://github.com/lucetius/rollbar-server.git',
   documentation: 'README.md'
@@ -9,6 +9,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   Npm.depends({ rollbar: '0.6.3' });
-  api.addFiles('rollbar-server.js');
+  api.addFiles('rollbar-server.js', 'server');
   api.export(['rollbar'], 'server');
 });
